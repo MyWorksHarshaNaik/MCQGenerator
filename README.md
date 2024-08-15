dc2ddb0b3934cc5fc5c11f6a70efdb5188a10408
MyWorksHarshaNaik <naikharshas235@gmail.com>
2024-08-15 16:13:55 +0530

    requirements added

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ conda create -n genproj python=3.10 -y
 ```bash
 conda activate genproj
 ```
+
+### Step 3: Install required packages
+```bash
+pip install -r requirements.txt 
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Harsha Naik',
+    author_email='harshanaik8197@gmail.com',
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2", "gemiini", "python-gemini-api"],
+    packages=find_packages()
+)
