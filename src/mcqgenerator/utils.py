00435575fad603ff575a03f6fcb44a6c5bcc9d7b
MyWorksHarshaNaik <naikharshas235@gmail.com>
2024-08-16 15:26:14 +0530

    small mistake solved

--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -31,17 +31,15 @@ def get_table_data(quiz_str):
         quiz_table_data = []
         for key, value in quiz_dict.items():
             mcq = value["MCQ"]
-            options = " | ".join(
+            options = " || ".join(
                 [
-                    f"{option}: {option_value}"
-                    for option, option_value in value["Options"].items()
+                    f"{option} -> {option_value}" for option, option_value in value["Options"].items()
                 ])
 
             correct = value["Correct"]
-            quiz_table_data.append(
-                {"MCQ": mcq, "Choices": options, "Correct": correct})
+            quiz_table_data.append({"MCQ": mcq, "Choices": options, "Correct": correct})
 
-            return quiz_table_data
+        return quiz_table_data
 
     except Exception as e:
         traceback.print_exception(type(e), e, e.__traceback__)
